c28e46f01b12a757c398bca67ab626c72784a5eb
Kevin Lobsinger <kevlobsinger@gmail.com>
2024-03-23 18:26:21 -0400

    Feat: add backend call to retrieve map and pin data for city

--- Frontend/src/components/Cityinfo.jsx
+++ Frontend/src/components/Cityinfo.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Text, VStack } from '@chakra-ui/react';
-import { GoogleMap, LoadScript, LoadScriptNext, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScriptNext, Marker } from '@react-google-maps/api';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 import Weather from './Weather';
-
+import MapCard from './Mapcard';
 
 const CityDetail = () => {
   const [city, setCity] = useState(null);
+  const [map, setMap] = useState(null);
   const { cityId } = useParams();
   const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
@@ -23,6 +24,21 @@ const CityDetail = () => {
 
     fetchCityData();
   }, [cityId]);
+
+  useEffect(() => {
+    const fetchMapData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/api/maps/${cityId}`);
+        console.log(response.data[0]);
+        setMap(response.data[0]);
+      } catch (error) {
+        console.error('Failed to fetch map data:', error);
+      }
+    };
+
+    fetchMapData();
+  }, [cityId]);
+
 
   if (!city) {
     return <Text>Loading city details...</Text>;
@@ -49,11 +65,11 @@ const CityDetail = () => {
             center={center}
             zoom={city.zoom_level}
           >
-            <Marker position={center} />
           </GoogleMap>
         </LoadScriptNext>
         {city && <Weather latitude={city.latitude} longitude={city.longitude} />}
       </Box>
+      {map && <MapCard map={map} />}
     </VStack>
   );
 };
